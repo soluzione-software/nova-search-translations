6e364793a77198037b18d274bd84bfcd9cf495bf
Yuriy Martini <y.martini@soluzionesoftware.com>
2019-07-10 14:52:53 +0200

    First implementation.

--- src/SearchesTranslations.php
+++ src/SearchesTranslations.php
@@ -2,7 +2,42 @@
 
 namespace SoluzioneSoftware\Nova\SearchTranslations;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
 trait SearchesTranslations
 {
-//    todo:
+    /**
+     * The translated columns that should be searched.
+     *
+     * @var array
+     */
+    public static $searchTranslations = [];
+
+    /**
+     * Get the searchable translated columns for the resource.
+     *
+     * @return array
+     */
+    public static function searchableTranslatedColumns()
+    {
+        return static::$searchTranslations;
+    }
+
+    /**
+     * Apply the search query to the query.
+     *
+     * @param  Builder  $query
+     * @param  string  $search
+     * @return Builder
+     */
+    protected static function applyTranslatedSearch($query, $search)
+    {
+        return $query->orWhere(function ($query) use ($search) {
+            foreach (static::searchableTranslatedColumns() as $column) {
+                $query->orWhereTranslationLike($column, '%'.$search.'%');
+            }
+        });
+    }
+
 }
